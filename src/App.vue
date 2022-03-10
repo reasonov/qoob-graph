f9590feaa72ec59aa0ab0272978ec2b9e8be2b3f
reasonov1k <iluayout@gmail.com>
2022-03-10 14:35:11 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main-layout />
+</template>
+
+<script>
+import mainLayout from "@/layouts/main-layout";
+
+export default {
+  name: 'App',
+  components: {
+    mainLayout: mainLayout
+  }
+}
+</script>
+
+<style src="./assets/fonts/style.css"></style>
+<style src="./assets/css/main.css"></style>
---
+++ src/components/data/itemsContent.js
@@ -0,0 +1,51 @@
+export default function() {
+    const contentList = [
+        {
+            name: 'Чемпионы',
+            number: '4180',
+            percent: '38'
+        },
+        {
+            name: 'Лояльные',
+            number: '1650',
+            percent: '15'
+        },
+        {
+            name: 'Растущие',
+            number: '2310',
+            percent: '21'
+        },
+        {
+            name: 'Новички',
+            number: '300',
+            percent: '4'
+        },
+        {
+            name: 'В зоне риска',
+            number: '776',
+            percent: '5'
+        },{
+            name: 'Средние на грани',
+            number: '1403',
+            percent: '13'
+        },
+        {
+            name: 'Сомневающиеся',
+            number: '666',
+            percent: '6'
+        },
+        {
+            name: 'Требуют внимания',
+            number: '330',
+            percent: '3'
+        },
+        {
+            name: 'Спящие',
+            number: '190',
+            percent: '2'
+        }
+
+    ]
+
+    return contentList
+}
